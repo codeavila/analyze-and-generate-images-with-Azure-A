de4e7106477c36218390a7dd03981fd6a51d6875
Eric Avila <cirexuab@gmail.com>
2023-11-16 21:44:19 -0600

    added : title, textbox,trigger

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [inputValue, setInputValue] = useState(''); // Estado para la URL o el prompt
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value); // Actualiza el estado con el valor del input
+  };
+
+  const handleAnalyzeImage = () => {
+    // Aquí irá la lógica para analizar la imagen
+    console.log('Analyze image with URL:', inputValue);
+  };
+
+  const handleGenerateImage = () => {
+    // Aquí irá la lógica para generar la imagen
+    console.log('Generate image with prompt:', inputValue);
+  };
+
+  return (
+    <div className="App">
+      <h1>Image Analysis and Generation</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter image URL or prompt"
+      />
+      <button onClick={handleAnalyzeImage}>Analyze Image</button>
+      <button onClick={handleGenerateImage}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
